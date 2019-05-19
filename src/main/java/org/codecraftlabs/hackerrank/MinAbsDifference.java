ee413a1e1701cda4567ed0f72be47b54ee5edf11
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2019-05-19 17:16:47 +0200

    more code

--- src/main/java/org/codecraftlabs/hackerrank/MinAbsDifference.java
+++ src/main/java/org/codecraftlabs/hackerrank/MinAbsDifference.java
@@ -1,21 +1,19 @@
 package org.codecraftlabs.hackerrank;
 
 
+import java.util.Arrays;
+
 public class MinAbsDifference {
     public int minimumAbsoluteDifference(int[] arr) {
-        int minimumAbsDiff = 0;
+        Arrays.sort(arr);
+        int minimumAbsDiff = Integer.MAX_VALUE;
         int length = arr.length;
 
-        for (int i = 0; i < length; i++) {
-            for (int j = i + 1; j < length; j++) {
-                int difference = Math.abs(arr[i] - arr[j]);
-
-                if (j == 1) {
-                    minimumAbsDiff = difference;
-                } else if (difference < minimumAbsDiff) {
-                    minimumAbsDiff = difference;
-                }
+        for (int i = 0; i < length - 1; i++) {
+            if (Math.abs(arr[i] - arr[i + 1]) < minimumAbsDiff) {
+                minimumAbsDiff = Math.abs(arr[i] - arr[i + 1]);
             }
+
         }
         return minimumAbsDiff;
     }
