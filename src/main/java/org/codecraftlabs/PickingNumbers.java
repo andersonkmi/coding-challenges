394516dd266f64f3903b273d7da9d720aa70ab56
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2019-09-22 17:41:35 +0200

    removed unnecessary logic

--- src/main/java/org/codecraftlabs/PickingNumbers.java
+++ src/main/java/org/codecraftlabs/PickingNumbers.java
@@ -1,25 +1,17 @@
 package org.codecraftlabs;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class PickingNumbers {
     public int pickingNumbers(List<Integer> numbers) {
-        // Sort the list first
-        Collections.sort(numbers);
-
         // Extract the distinct values and create the pairs
         List<Integer> distinctNumbers = numbers.stream().distinct().collect(Collectors.toList());
 
         // Iterate and create the sets
         int maxSize = 0;
-        for(int index = 0; index < distinctNumbers.size(); index++) {
-            Integer currentValue = distinctNumbers.get(index);
-
-            List<Integer> selectedValues = numbers.stream().filter(item -> item == currentValue || item == currentValue + 1).collect(Collectors.toList());
+        for (Integer currentValue : distinctNumbers) {
+            List<Integer> selectedValues = numbers.stream().filter(item -> item.equals(currentValue) || item.equals(currentValue + 1)).collect(Collectors.toList());
             if (selectedValues.size() > maxSize) {
                 maxSize = selectedValues.size();
             }
