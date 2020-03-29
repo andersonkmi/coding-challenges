f1d2937f56ec1e63823c82625eea28eb40084da7
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-03-29 23:46:13 +0200

    more code

--- src/main/java/org/codecraftlabs/MaxArraySum.java
+++ src/main/java/org/codecraftlabs/MaxArraySum.java
@@ -1,7 +1,9 @@
 package org.codecraftlabs;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MaxArraySum {
     public int maxSubsetSum(int[] arr) {
@@ -15,9 +17,33 @@ public class MaxArraySum {
 
     }
 
+    public Map<Integer, List<Integer>> createMap(int length) {
+        Map<Integer, List<Integer>> results = new HashMap<>();
+        for (int val = 0; val < length; val++) {
+            List<Integer> values = listItems(val, length);
+            if (!values.isEmpty()) {
+                results.put(val, listItems(val, length));
+            }
+        }
+        return results;
+    }
+
+    private List<Integer> listItems(int currentValue, int length) {
+        List<Integer> validPositions = new ArrayList<>();
+        int position = currentValue;
+        while (position < length) {
+            if (position - currentValue >= 2) {
+                validPositions.add(position);
+            }
+            position++;
+        }
+        return validPositions;
+    }
+
     public static void main(String[] args) {
         MaxArraySum service = new MaxArraySum();
-        int[] items = {3, 7, 4, 6, 5};
-        service.maxSubsetSum(items);
+        int length = 6;
+        Map<Integer, List<Integer>> items = service.createMap(length);
+        System.out.println(items);
     }
 }
